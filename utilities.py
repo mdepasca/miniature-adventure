8ce1f7659df78ad2e018c2da3a74429b9d51422c
mdepasca <mdepasca@eso.org>
2014-08-21 17:42:38 +0200

    Added dump functions and more text colors.

--- utilities.py
+++ utilities.py
@@ -3,7 +3,7 @@ Implementation of general use functions.
 """
 import numpy as np
 import numpy.ma as ma
-import pickle 
+import cPickle 
 import gzip 
 import GPy 
 import classes 
@@ -27,12 +27,83 @@ class Capturing(list):
         sys.stdout = self._stdout
 
 class bcolors:
+    txtblk='\033[0;30m' # Black - Regular
+    txtred='\033[0;31m' # Red
+    txtgrn='\033[0;32m' # Green
+    txtylw='\033[0;33m' # Yellow
+    txtblu='\033[0;34m' # Blue
+    txtpur='\033[0;35m' # Purple
+    txtcyn='\033[0;36m' # Cyan
+    txtwht='\033[0;37m' # White
+
+    bldblk='\033[1;30m' # Black - Bold
+    bldred='\033[1;31m' # Red
+    bldgrn='\033[1;32m' # Green
+    bldylw='\033[1;33m' # Yellow
+    bldblu='\033[1;34m' # Blue
+    bldpur='\033[1;35m' # Purple
+    bldcyn='\033[1;36m' # Cyan
+    bldwht='\033[1;37m' # White
+
+    unkblk='\033[4;30m' # Black - Underline
+    undred='\033[4;31m' # Red
+    undgrn='\033[4;32m' # Green
+    undylw='\033[4;33m' # Yellow
+    undblu='\033[4;34m' # Blue
+    undpur='\033[4;35m' # Purple
+    undcyn='\033[4;36m' # Cyan
+    undwht='\033[4;37m' # White
+
+    bakblk='\033[40m'   # Black - Background
+    bakred='\033[41m'   # Red
+    badgrn='\033[42m'   # Green
+    bakylw='\033[43m'   # Yellow
+    bakblu='\033[44m'   # Blue
+    bakpur='\033[45m'   # Purple
+    bakcyn='\033[46m'   # Cyan
+    bakwht='\033[47m'   # White
+    txtrst='\033[0m'    # Text Reset
+    
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
+
+
+def open_pkl(filePath):
+    # print '>>> Loading pkl file from: ' + filePath
+    # elTime = time.time()
+    
+    fileHandler = open(filePath,'r')
+    data = cPickle.load(fileHandler)
+    fileHandler.close()
+
+    # elTime -= time.time()
+    # print '>>> Done in ' + str(abs(elTime)) + ' secs'
+    return data
+
+def dump_pkl(filePath, dataStruct):
+    # print '>>> Dumping data structure into: ' + filePath
+    # ERROR_STATE = 0
+    whileOn = True
+    i = 0
+    while whileOn:
+        try:
+            with open(filePath):
+                i += 1
+                pklIdx = filePath.rfind('.pkl')
+                if i > 1: pklIdx -= 3
+                filePath = filePath[0:pklIdx] + '({:<1}).pkl'.format(i)
+        except IOError:
+            whileOn = False            
+
+    fileHandler = open(filePath,'w')
+        
+    cPickle.dump(dataStruct, fileHandler)
+    fileHandler.close()
+
 
 if __name__ == '__main__':
     """
@@ -270,7 +341,7 @@ def gp_fit(
         gpModel.optimize(optimizer='scg')
 
     # predX = reshape_for_GPy(np.arange(round(X.min()), round(X.max()), 1))
-    predX = reshape_for_GPy(np.arange(X.min(), X.max(), 1))
+    predX = reshape_for_GPy(np.arange(X.min(), X.max(), 1.))
     # predX = reshape_for_GPy(np.linspace(round(X.min()), round(X.max()), num=100))
 
     meanY, var = gpModel._raw_predict(predX, full_cov=False)
