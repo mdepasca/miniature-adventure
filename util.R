d0c614f8a77ecc1d16e005e61da695e6a0dc7d62
mdepasca <mdepasca@eso.org>
2015-02-17 15:21:01 +0100

    Implemented exploration of light curve duration.

--- util.R
+++ util.R
@@ -77,30 +77,44 @@ explore.lightcurves <- function(dir.path, band='r', skip=63, err.bars=FALSE, mul
     ## return(file.list)
 }
 
-explore.minmax <- function(dir.path, band='r', skip=63){
+explore.minmax <- function(dir.path, band='r', save=FALSE){
     mjd.min.vec <- c()
     mjd.max.vec <- c()
     mjd.flux.max.vec <- c()
     idx.flux.max.vec <- c()
+    ext.mjd <- c()
     file.list <- list.files(path=dir.path, pattern='\\.DAT$', full.names=TRUE)
     for (f in file.list){
-        ## message(f)
+        # message(f)
+        r <- readLines(f)
+        skip <- grep('NVAR:', r)
         lc <- read.table(f, header=TRUE, skip=skip, fill=TRUE)
-        del.rows <- which(is.na(lc$FLUXCALERR))
-        lc <- lc[-del.rows,]
-        lc$FLUXCAL <- as.numeric(levels(lc$FLUXCAL))[lc$FLUXCAL]
+        if (class(lc$FLUXCAL) != 'numeric'){
+            del.rows <- which(is.na(lc$FLUXCALERR))
+            lc <- lc[-del.rows,]
+            lc$FLUXCAL <- as.numeric(levels(lc$FLUXCAL))[lc$FLUXCAL]
+        }
         mjd.min.vec <- c(mjd.min.vec, min(lc$MJD))
         mjd.max.vec <- c(mjd.max.vec, max(lc$MJD))
+        ext.mjd <- c(ext.mjd, (max(lc$MJD)-min(lc$MJD)))
         mjd.flux.max.vec <- c(mjd.flux.max.vec, lc$MJD[which(lc$FLUXCAL == max(lc$FLUXCAL), arr.ind=TRUE)])
         idx.flux.max.vec <- c(idx.flux.max.vec, which(lc$FLUXCAL == max(lc$FLUXCAL), arr.ind=TRUE))
     }
+    ext.mjd <- ext.mjd[!is.na(ext.mjd)]
     d.mjd <- density(mjd.flux.max.vec)
     d.idx <- density(idx.flux.max.vec)
-    par(mfrow=c(2,1))
+    d.ext <- density(ext.mjd)
+    if (save){
+        png(filename='results/plots/mjd_distributions.png', width=7, height= 7, units='in', res=300)
+    }
+    par(mfrow=c(3,1))
     plot(d.mjd, main='Distribution of epoch of maximum light', xlab='epoch [MJD]')
     rug(c(min(mjd.min.vec), max(mjd.max.vec), median(mjd.flux.max.vec)))
     plot(d.idx, main='Distribution of index of maximum light')
+    plot(d.ext, main='Distribution of light curves duration', xlab='epoch [MJD]')
+
     par(mfrow=c(1,1))
+    if (save){dev.off()}
     message('MIN MJD   MAX MJD    MIN MJD MAX    MAX MJD MAX ')
-    return(c(min(mjd.min.vec), max(mjd.max.vec), min(mjd.flux.max.vec), max(mjd.flux.max.vec)))
+    return(c(min(mjd.min.vec), max(mjd.max.vec), min(mjd.flux.max.vec), max(mjd.flux.max.vec), ext.mjd))
 }
