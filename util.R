3e9b28b8c9cb1b61d65d8f5dc5482883e3e3fc52
mdepasca <mdepasca@eso.org>
2015-03-09 11:57:55 +0100

    Introduced function to compare redshift distribution in the 3 types.

--- util.R
+++ util.R
@@ -1,23 +1,43 @@
 library(Hmisc)
+library(sm) # for comparative density plots.
+library(plotrix) # boxed labels
 
-redshift.distrib <- function(dump.file,save=FALSE){
-    dump <- read.table(dump.file, header=TRUE, skip=1)
+redshift.distrib <- function(dump.df, save=FALSE){
+    # dump <- read.table(dump.file, header=TRUE, skip=1)
+    par(mfrow=c(1,1))
+    z.ref <- d$x[which(d$y==max(d$y))]
+    z.thr <- 0.17
 
-    if (is.name(dump$Z)){
-        d <- density(dump$Z)
-    }else {
-        d <- density(dump$GENZ)
-    }
+    d <- density(dump.df$GENZ, bw=0.03)
     if (save){
-        message('saving plot on file redshift_distribution.png...')
-        png(filename='redshift_distribution.png', width=4, height= 4, units='in', res=300)
-        plot(d, main='Redshift distribution')
+        message('saving plot on file...')
+        pdf(file='redshift_distribution.pdf', width=7, height= 7)
+    }
+    # plot.new()
+    plot(d, xlab='Redshift', main='')#, sub=paste('N =', length(dump.df$CID), 
+        # ' Bandwidth =', d$bw, sep=' '))
+    polygon(c(0,d$x[which(d$x<=z.thr)],z.thr), c(0,d$y[which(d$x<=z.thr)],0),
+        density=10, angle=45)
+    boxed.labels(0.1, 0.1, labels=paste(length(
+        dump.df$GENZ[which(dump.df$GENZ<z.thr)])), cex=1, border=FALSE)
+
+    polygon(c(z.ref-z.thr/2, d$x[which((z.ref-z.thr/2)<d$x & 
+        d$x<(z.ref+z.thr/2))], z.ref+z.thr/2), 
+    c(0,d$y[which((z.ref-z.thr/2)<d$x & d$x<(z.ref+z.thr/2))],0), 
+    density=10, angle=135, lty=1)
+    boxed.labels(z.ref, 0.7, labels=paste(length(
+        dump.df$GENZ[which((z.ref-z.thr/2)<dump.df$GENZ & 
+        dump.df$GENZ<(z.ref+z.thr/2))])), cex=1, border=FALSE, ypad=1.5)
+        # col='darkgray', border='darkgray')
+    # lines(d, xlab='redshift', main='', sub=paste('N =', length(dump.df$CID), 
+    #     ' Bandwidth =', d$bw, sep=' '))
+        
+    rug(d$x[which(d$y==max(d$y))])
+    if (save){
         dev.off()
         message('done!')
-    }else{
-        plot(d, main='Redshift distribution')
     }
-    return(dump)
+    return(d)
 }
 
 explore.lightcurves <- function(dir.path, band='r', skip=63, err.bars=FALSE, multi.plot=FALSE, save=FALSE){
@@ -117,4 +137,103 @@ explore.minmax <- function(dir.path, band='r', save=FALSE){
     if (save){dev.off()}
     message('MIN MJD   MAX MJD    MIN MJD MAX    MAX MJD MAX ')
     return(c(min(mjd.min.vec), max(mjd.max.vec), min(mjd.flux.max.vec), max(mjd.flux.max.vec), ext.mjd))
+}
+
+get.dump <- function(dump.file){
+    dump <- read.table(dump.file, header=TRUE, skip=1)
+    dump$GENTYPE[which(dump$GENTYPE == 21)] <- 2
+    dump$GENTYPE[which(dump$GENTYPE == 22)] <- 2
+    dump$GENTYPE[which(dump$GENTYPE == 23)] <- 2
+
+    dump$GENTYPE[which(dump$GENTYPE == 32)] <- 3
+    dump$GENTYPE[which(dump$GENTYPE == 33)] <- 3
+
+    dump$GENTYPE <- as.factor(dump$GENTYPE)
+
+    return(dump)
+}
+
+compare.densities <- function(dump.df,save=FALSE, train=FALSE){
+    if (save){
+        pdf(file='compare_redshift_distribution.pdf', width=7, height= 7)
+    }
+    gentype.f <- factor(dump.df$GENTYPE, levels=c(1,2,3), 
+        labels=c("type Ia", "type II", "type Ib/c"))
+    d <- density(dump.df$GENZ, bw=0.03)
+    if (train){
+        train.df <- read.table('results/SIMGEN_PUBLIC_DES.TRAIN', 
+            col.names=c('idx', 'snid', 'path', 'type'))
+        dump.df <- dump.df[match(train.df$snid, dump.df$CID),]
+    }
+    
+    d1 <- density(dump.df$GENZ[which(dump.df$GENTYPE == 1)], bw=0.03)
+    d2 <- density(dump.df$GENZ[which(dump.df$GENTYPE == 2)], bw=0.03)
+    d3 <- density(dump.df$GENZ[which(dump.df$GENTYPE == 3)], bw=0.03)
+
+    z.ref <- d$x[which(d$y==max(d$y))]
+    z.thr <- 0.17/2
+
+    colfill <- c('darkorange', 'seagreen3', 'blue2')
+    par(mfrow=c(3,1), lty=3, ann=FALSE, mar=c(0,0,0,0), oma=c(5,6,4,2))
+    plot(d3$x, d3$n*d3$y, col=colfill[3], main='', xlab='Redshift', xlim=c(0,max(d1$x)), 
+        ylim=c(0,3.5), axes=FALSE, type='l')
+    # plot(d3, col=colfill[3], main='', xlab='Redshift', xlim=c(0,max(d1$x)), 
+    #     ylim=c(0,3.5), axes=FALSE)
+    axis(1, at=seq(0,max(d1$x),0.2), label=FALSE, cex.axis=1.5)
+    axis(2, at=seq(0,3.5,1), cex.axis=1.5)
+
+    box()
+    polygon(c(z.ref-z.thr,d3$x[which((z.ref-z.thr)<d3$x & 
+        d3$x<(z.ref+z.thr))],z.ref+z.thr), 
+    c(0,d3$y[which((z.ref-z.thr)<d3$x & d3$x<(z.ref+z.thr))],0), 
+    col=colfill[3], border=colfill[3], density=10, angle=45, lty=1)
+    mtext("type Ib/c", side=3, line=-2, adj=0.9)
+    mtext(paste("N = ", length(which(dump.df$GENTYPE == 3))), line=-4, adj=0.9)
+    boxed.labels(z.ref-.01, 0.5, labels=paste(length(dump.df$GENZ[which((dump.df$GENTYPE == 3) 
+        & (z.ref-z.thr)<dump.df$GENZ & dump.df$GENZ<(z.ref+z.thr))])), cex=1, border=FALSE)
+
+    par(lty=2)
+    plot(d2,col=colfill[2], main='', xlab='Redshift', xlim=c(0,max(d1$x)), 
+        ylim=c(0,3.5), axes=FALSE)
+    axis(1, at=seq(0,max(d1$x),0.2), label=FALSE, cex.axis=1.5)
+    axis(2, at=seq(0,3.5,1), cex.axis=1.5)
+    box()
+    polygon(c(z.ref-z.thr,d2$x[which((z.ref-z.thr)<d2$x & 
+        d2$x<(z.ref+z.thr))],z.ref+z.thr), 
+    c(0,d2$y[which((z.ref-z.thr)<d2$x & d2$x<(z.ref+z.thr))],0), 
+    col=colfill[2], border=colfill[2], density=10, angle=45, lty=1)
+    mtext("type II", side=3, line=-2, adj=0.9)
+    mtext(paste("N = ", length(which(dump.df$GENTYPE == 2))), line=-4, adj=0.93)
+    boxed.labels(z.ref, 0.5, labels=paste(length(dump.df$GENZ[which((dump.df$GENTYPE == 2) 
+        & (z.ref-z.thr)<dump.df$GENZ & dump.df$GENZ<(z.ref+z.thr))])), cex=1, border=FALSE)
+
+    par(lty=1)
+    plot(d1, col=colfill[1], main='', xlab='Redshift', xlim=c(0,max(d1$x)),
+        ylim=c(0,3.5), axes=FALSE)
+    axis(1, at=seq(0,max(d1$x),0.2), cex.axis=1.5)
+    axis(2, at=seq(0,3.5,1), cex.axis=1.5)
+    box()
+    polygon(c(z.ref-z.thr,d1$x[which((z.ref-z.thr)<d1$x & 
+        d1$x<(z.ref+z.thr))],z.ref+z.thr), 
+    c(0,d1$y[which((z.ref-z.thr)<d1$x & d1$x<(z.ref+z.thr))],0), 
+    col=colfill[1], border=colfill[1], density=10, angle=45, lty=1)
+
+    rug(z.ref)
+
+    boxed.labels(z.ref, 0.5, labels=paste(length(dump.df$GENZ[which((dump.df$GENTYPE == 1) 
+        & (z.ref-z.thr)<dump.df$GENZ & dump.df$GENZ<(z.ref+z.thr))])), cex=1, border=FALSE)
+    mtext("type Ia", side=3, line=-2, adj=0.9)
+    mtext(paste("N = ", length(which(dump.df$GENTYPE == 1))), line=-4, adj=0.92)
+    mtext("Redshift", side=1, outer=TRUE, line=3.2)
+    mtext("Density", side=2, outer=TRUE, line=3.5)
+    # sm.density.compare(dump.df$GENZ, dump.df$GENTYPE, h=0.03, xlab="redshift")
+    # if (train){
+    #     legend(0.8, 3, levels(gentype.f), fill=colfill)
+    # }
+    # else{
+    #     legend(0, 1.8, levels(gentype.f), fill=colfill)
+    # }
+    if (save){
+        dev.off()
+    }
 }
