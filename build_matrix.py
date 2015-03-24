3c13db5f654be92fd514a62f2ae58a3dd7e16c5f
mdepasca <mdepasca@eso.org>
2015-03-24 15:39:34 +0100

    Set the correct path to files.

--- build_matrix.py
+++ build_matrix.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 print 'mat00 ...'
-mat00 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424728136.080.txt')
+mat00 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424728136.080.txt')
 
 print 'mat01 ...'
-mat01 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424761931.836.txt')
+mat01 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424761931.836.txt')
 
 print 'mat02 ...'
-mat02 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424836678.486.txt')
+mat02 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424836678.486.txt')
 
 print 'mat03 ...'
-mat03 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426935010.715.txt')#dist_matrix_Sum_mothra_1424835453.155.txt')
+mat03 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426935010.715.txt')#dist_matrix_Sum_mothra_1424835453.155.txt')
 
 #---> hstacking ....
 print 'hstacking mat0 ...'
@@ -21,13 +21,13 @@ mat10 = np.transpose(mat01)
 # ----- line 1 -----
 
 print 'mat11 ...'
-mat11 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424976703.216.txt')
+mat11 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424976703.216.txt')
 
 print 'mat12 ...'
-mat12 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1425008330.511.txt')
+mat12 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1425008330.511.txt')
 
 print 'mat13 ...'
-mat13 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426934839.968.txt')#dist_matrix_Sum_mothra_1425030478.363.txt')
+mat13 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426934839.968.txt')#dist_matrix_Sum_mothra_1425030478.363.txt')
 
 #---> hstacking ....
 print 'stacking mat1 ...'
@@ -41,10 +41,10 @@ print 'mat21 ...'
 mat21 = np.transpose(mat12)
 
 print 'mat22 ...'
-mat22 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424934904.523.txt')
+mat22 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424934904.523.txt')
 
 print 'mat23 ...'
-mat23 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426935152.075.txt')#dist_matrix_Sum_mothra_1425005559.641.txt')
+mat23 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426935152.075.txt')#dist_matrix_Sum_mothra_1425005559.641.txt')
 
 #---> stacking ....
 
@@ -64,7 +64,7 @@ print 'mat32 ...'
 mat32 = np.transpose(mat23)
 
 print 'mat33 ...'
-mat33 = np.loadtxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424972850.416.txt')
+mat33 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424972850.416.txt')
 
 #---> stacking ....
 print 'stacking mat3 ...'
@@ -75,7 +75,7 @@ print 'stacking mat ...'
 mat = np.vstack((mat0, mat1, mat2, mat3))
 
 print 'saveing mat ...'
-np.savetxt('results/RBF_test-length/distance_matrix/dist_matrix_Sum.txt', mat)
+np.savetxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum.txt', mat)
 
 print 'END'
 #END
