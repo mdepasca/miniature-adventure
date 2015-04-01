dc541d6806d302ba7a1bf118c0083a4357b340b1
mdepasca <mdepasca@eso.org>
2015-04-01 17:06:38 +0200

    Added parameters to make it less hard-wired: path and input file with time stamps.

--- build_matrix.py
+++ build_matrix.py
@@ -1,81 +1,135 @@
 import numpy as np
+import gc
+import argparse
 
-print 'mat00 ...'
-mat00 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424728136.080.txt')
-
-print 'mat01 ...'
-mat01 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424761931.836.txt')
-
-print 'mat02 ...'
-mat02 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424836678.486.txt')
-
-print 'mat03 ...'
-mat03 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426935010.715.txt')#dist_matrix_Sum_mothra_1424835453.155.txt')
-
-#---> hstacking ....
-print 'hstacking mat0 ...'
-mat0 = np.hstack((mat00, mat01, mat02, mat03))
-
-print 'mat10 ...'
-mat10 = np.transpose(mat01)
-# ----- line 1 -----
-
-print 'mat11 ...'
-mat11 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424976703.216.txt')
-
-print 'mat12 ...'
-mat12 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1425008330.511.txt')
-
-print 'mat13 ...'
-mat13 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426934839.968.txt')#dist_matrix_Sum_mothra_1425030478.363.txt')
-
-#---> hstacking ....
-print 'stacking mat1 ...'
-mat1 = np.hstack((mat10, mat11, mat12, mat13))
-
-# ----- line 2 -----
-print 'mat20 ...'
-mat20 = np.transpose(mat02)
-
-print 'mat21 ...'
-mat21 = np.transpose(mat12)
-
-print 'mat22 ...'
-mat22 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424934904.523.txt')
-
-print 'mat23 ...'
-mat23 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1426935152.075.txt')#dist_matrix_Sum_mothra_1425005559.641.txt')
-
-#---> stacking ....
-
-print 'stacking mat2 ...'
-mat2 = np.hstack((mat20, mat21, mat22, mat23))
+if __name__ == "__main__":
+	"""
+	The routine works by reading in a file containig in each line the 
+	time stamp of the sub-matrix file (WARNING: they have to be in the 
+	correct order, 1st line -> sub-matrix 00; 2nd line -> sub-matrix 01 
+	etc.).
+	The time stamp is inserted in the file name.
+	"""
 
 
-# ----- line 3 -----
+	parser = argparse.ArgumentParser(
+        description = "SN lightcurve fitter and classifier: Bulding distance matrix from files.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-print 'mat30 ...'
-mat30 = np.transpose(mat03)
+	parser.add_argument(
+		'--path', '-p', dest=path,
+		help='Path where to find files.')
 
-print 'mat31 ...'
-mat31 = np.transpose(mat13)
+	parser.add_argument(
+		'--time-stamps', '-t', dest=timeStamps,
+		default=''
+		help='ASCII file containing time stamps of sub-matrix files. One per line in the correct order.')
 
-print 'mat32 ...'
-mat32 = np.transpose(mat23)
+	args = parser.parse_args()
 
-print 'mat33 ...'
-mat33 = np.loadtxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum_mothra_1424972850.416.txt')
+if __name__ == "__main__":
+	if args.timeStamps == '':
+		raise SystemExit
 
-#---> stacking ....
-print 'stacking mat3 ...'
-mat3 = np.hstack((mat30, mat31, mat32, mat33))
+	inFile = file(args.timeStamps, 'r')
+	timeStamps = inFile.readlines()
+	inFile.close()
 
-#---> stacking ....
-print 'stacking mat ...'
-mat = np.vstack((mat0, mat1, mat2, mat3))
+	# path = 'results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/'
+	fileName = 'dist_matrix_Sum_mothra_{:<14.3f}.txt'
 
-print 'saveing mat ...'
-np.savetxt('results/SIMGEN_PUBLIC_FIT/RBF_test-length/distance_matrix/dist_matrix_Sum.txt', mat)
+	print 'mat00 ...'
+	mat00 = np.loadtxt(args.path+fileName.format(timeStamps[0]))
 
-print 'END'
-#END
+	print 'mat01 ...'
+	mat01 = np.loadtxt(args.path+fileName.format(timeStamps[1]))
+
+	print 'mat02 ...'
+	mat02 = np.loadtxt(args.path+fileName.format(timeStamps[2]))
+
+	print 'mat03 ...'
+	mat03 = np.loadtxt(args.path+fileName.format(timeStamps[3]))
+
+	#---> hstacking ....
+	print 'hstacking mat0 ...'
+	mat0 = np.hstack((mat00, mat01, mat02, mat03))
+
+	del mat00, mat01, mat02, mat03
+	gc.collect()
+
+	print 'mat10 ...'
+	mat10 = np.transpose(mat01)
+	# ----- line 1 -----
+
+	print 'mat11 ...'
+	mat11 = np.loadtxt(args.path+fileName.format(timeStamps[4]))
+
+	print 'mat12 ...'
+	mat12 = np.loadtxt(args.path+fileName.format(timeStamps[5]))
+
+	print 'mat13 ...'
+	mat13 = np.loadtxt(args.path+fileName.format(timeStamps[6]))
+
+	#---> hstacking ....
+	print 'stacking mat1 ...'
+	mat1 = np.hstack((mat10, mat11, mat12, mat13))
+
+	del mat10, mat11, mat12, mat13
+	gc.collect()
+
+	# ----- line 2 -----
+	print 'mat20 ...'
+	mat20 = np.transpose(mat02)
+
+	print 'mat21 ...'
+	mat21 = np.transpose(mat12)
+
+	print 'mat22 ...'
+	mat22 = np.loadtxt(args.path+fileName.format(timeStamps[7]))
+
+	print 'mat23 ...'
+	mat23 = np.loadtxt(args.path+fileName.format(timeStamps[8]))
+
+	#---> stacking ....
+
+	print 'stacking mat2 ...'
+	mat2 = np.hstack((mat20, mat21, mat22, mat23))
+
+	del mat20, mat21, mat22, mat23
+	gc.collect()
+
+	# ----- line 3 -----
+
+	print 'mat30 ...'
+	mat30 = np.transpose(mat03)
+
+	print 'mat31 ...'
+	mat31 = np.transpose(mat13)
+
+	print 'mat32 ...'
+	mat32 = np.transpose(mat23)
+
+	print 'mat33 ...'
+	mat33 = np.loadtxt(args.path+fileName.format(timeStamps[9]))
+
+	#---> stacking ....
+	print 'stacking mat3 ...'
+	mat3 = np.hstack((mat30, mat31, mat32, mat33))
+
+	del mat30, mat31, mat32, mat33
+	gc.collect()
+
+	#---> stacking ....
+	print 'stacking mat ...'
+	mat = np.vstack((mat0, mat1, mat2, mat3))
+
+	del mat0, mat1, mat2, mat3
+	gc.collect()
+
+	print 'saveing mat ...'
+	np.savetxt(args.path+'dist_matrix_Sum.txt', mat)
+
+	del mat
+	gc.collect()
+	print 'END'
+	#END
