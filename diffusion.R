b2b7d3fc2de69e7e308f093076a52128a668d08c
mdepasca <mdepasca@eso.org>
2015-04-09 14:37:29 +0200

    Modified path to distance matrix file.

--- diffusion.R
+++ diffusion.R
@@ -2,7 +2,7 @@
 library(diffusionMap)
 library(R.utils)
 
-filePath <- 'results/RATQUAD-with_prior/distance-matrix/dist_matrix_Sum.txt'
+filePath <- 'results/SIMGEN_PUBLIC_FIT/RATQUAD-with_prior/distance-matrix/dist_matrix_Sum.txt'
 fileLines <- countLines(filePath)
 distMat <- matrix(scan(filePath, comment.char='#'),
     ncol=fileLines, nrow=fileLines, byrow=TRUE)
