36a6e7f8c4b4d251eea678f1000739f6d4de713a
mdepasca <mdepasca@eso.org>
2015-04-09 15:11:39 +0200

    Defined missing `eps.grid`.

--- diffusion.R
+++ diffusion.R
@@ -3,8 +3,15 @@ library(diffusionMap)
 library(R.utils)
 
 filePath <- 'results/SIMGEN_PUBLIC_FIT/RATQUAD-with_prior/distance_matrix/dist_matrix_Sum.txt'
-fileLines <- countLines(filePath)
-distMat <- matrix(scan(filePath, comment.char='#'),
-    ncol=fileLines, nrow=fileLines, byrow=TRUE)
+if (!exists('fileLines')){
+    fileLines <- countLines(filePath)
+}
+
+eps.grid <- as.vector(seq(from=2, to=5, by=0.2))
+
+if (!exists('distMat')){
+    distMat <- matrix(scan(filePath, comment.char='#'),
+        ncol=fileLines, nrow=fileLines, byrow=TRUE)
+}
 
 dmap <- diffuse(distMat, eps.val=eps.grid[2], neigen=120)
