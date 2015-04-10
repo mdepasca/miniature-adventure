3d6ec9a972310aaa201d92072bcae414357aa6ab
mdepasca <mdepasca@eso.org>
2015-04-10 11:02:37 +0200

    Added code for coloured pair plot.

--- diffusion.R
+++ diffusion.R
@@ -10,8 +10,16 @@ if (!exists('fileLines')){
 eps.grid <- as.vector(seq(from=2, to=5, by=0.2))
 
 if (!exists('distMat')){
+    message(paste('Reading distance matrix from ', filePath, ' ...'))
     distMat <- matrix(scan(filePath, comment.char='#'),
         ncol=fileLines, nrow=fileLines, byrow=TRUE)
 }
 
+message('Creating diffusion map...')
 dmap <- diffuse(distMat, eps.val=eps.grid[2], neigen=120)
+
+dmap.corr <- abs(cor(dmap$X[,1:5]))
+myorder <- order.single(dmap.corr)
+mycolors <- dmat.color(dmap.corr)
+cpairs(dmap$X[,1:5], myorder, panel.colors=mycolors, gap=.5)
+# pairs(~dmap$X[,1]+dmap$X[,2]+dmap$X[,3]+dmap$X[,4]+dmap$X[,5]+dmap$X[,6])
