a2af9aa28067ce60e7c878e9090c192ca6dd0bc4
mdepasca <mdepasca@eso.org>
2015-04-13 15:48:55 +0200

    Added `sep=` to `paste` command.

--- diffuse_and_classify.R
+++ diffuse_and_classify.R
@@ -6,17 +6,17 @@ source('classify_RF.R')
 path <- 'results/SIMGEN_PUBLIC_FIT/'
 specific.path <- 'RATQUAD-with_prior'
 
-path <- paste(path, specific.path, '/distance_matrix/')
+path <- paste(path, specific.path, '/distance_matrix/', sep='')
 
 neigen <- 120
 eps.grid <- as.vector(seq(from=2, to=5, by=0.2))
 
 dmap <- calc_diffusion_map(path, eps.val=eps.grid[2], neigen=niegen)
 
-trainingSet <- get_training(path=paste(path,specific.path,'/'), 
+trainingSet <- get_training(path=paste(path,specific.path,'/', sep=''), 
 	fileNameRoot=specific.path)
 
-testSet <- get_test(path=paste(path,specific.path,'/'), 
+testSet <- get_test(path=paste(path,specific.path,'/', sep=''), 
 	fileNameRoot=specific.path)
 
 sn.rf <- classify_RF(dmap, trainingSet, testSet)
