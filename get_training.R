6e2eeb8068c7761af5cb1de123a79bd2ab12e798
mdepasca <mdepasca@eso.org>
2015-04-15 14:41:01 +0200

    Added messages, fixed regexp in `list.files`.

--- get_training.R
+++ get_training.R
@@ -1,16 +1,18 @@
 get_training <- function(path, fileNameRoot){
-	fNames <- list.files(path, 
-        pattern=paste(fileNameRoot, '\\.I[[:alnum:]]{1,2}.TRAIN', sep=''))
+
+    fNames <- list.files(path, pattern=paste(fileNameRoot, '\\.I[[:alnum:]]{1,2}\\.TRAIN', sep=''))
+    message(fNames)
     for (f in fNames){
-        message(f)
+        message(paste('Reading file', f))
     	if (!exists('trainingSet')){
-    		trainingSet <- read.table(paste(path,f, sep=''), 
-    			col.names=c('idx', 'snid', 'path', 'type'))
+            trainingSet <- read.table(paste(path,f, sep=''), 
+                                      col.names=c('idx', 'snid', 'path', 'type'))
     	}else{
-    		trainingSet <- rbind(trainingSet, read.table(paste(path,f, sep=''), 
-    			col.names=c('idx', 'snid', 'path', 'type')))
-    	}
+             trainingSet <- rbind(trainingSet, read.table(paste(path,f, sep=''), 
+                                                          col.names=c('idx', 'snid', 'path', 'type')))
+         }
     }
+    message('Increasing by 1 idx column')
     trainingSet$idx <- trainingSet$idx + 1
 
     return(trainingSet)
