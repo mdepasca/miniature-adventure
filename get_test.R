55d549b8fe485c827ff0ef3abcf894bcceb6e024
mdepasca <mdepasca@eso.org>
2015-04-15 17:27:18 +0200

    Reading `type` column from dump file.

--- get_test.R
+++ get_test.R
@@ -1,20 +1,29 @@
 source('util.R')
 
 get_test <- function(path, fileNameRoot){
-    testSet <- read.table(paste(path, fileNameRoot, '.TEST', sep=''), 
-                          col.names=c('idx', 'snid', 'path', 'type'))
+    ## column `type` is set to 'test' for each SN
+    testSet <- read.table(paste(path, fileNameRoot, '.TEST', sep=''),
+                          stringsAsFactors=FALSE,
+                          col.names=c('idx', 'snid', 'path'))
+    
     dump <- get_dump('train_data/SIMGEN_PUBLIC_DES/SIMGEN_PUBLIC_DES.DUMP')
 
-    match.idx <- match(testSet$snid,dump$CID)
-    testSet$type <- dump$GENTYPE[match.idx]
+    match.idx <- match(testSet$snid, dump$CID)
 
-    testSet$type[testSet$type == 1]  <- 'snIa'
-    testSet$type[testSet$type == 21] <- 'snII'
-    testSet$type[testSet$type == 22] <- 'snII'
-    testSet$type[testSet$type == 23] <- 'snII'
-    testSet$type[testSet$type == 3]  <- 'snIbc'
-    testSet$type[testSet$type == 32] <- 'snIbc'
-    testSet$type[testSet$type == 33] <- 'snIbc'
+    testSet <- cbind(testSet, dump$GENTYPE[match.idx])
+    colnames(testSet)[length(colnames(testSet))] <- 'type'
+
+    testSet$type <- sapply(testSet$type, as.numeric)
+        
+    testSet$type[which(testSet$type == 1 )] <- 'snIa'
+    testSet$type[which(testSet$type == 2 )] <- 'snII'
+    testSet$type[which(testSet$type == 21)] <- 'snII'
+    testSet$type[which(testSet$type == 22)] <- 'snII'
+    testSet$type[which(testSet$type == 23)] <- 'snII'
+    testSet$type[which(testSet$type == 3 )] <- 'snIbc'
+    testSet$type[which(testSet$type == 32)] <- 'snIbc'
+    testSet$type[which(testSet$type == 33)] <- 'snIbc'
+
     testSet$type <- as.factor(testSet$type)
     testSet$idx <- testSet$idx + 1
     
