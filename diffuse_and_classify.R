2de6acb7ef4152b3febb9646f67440e08b735b74
mdepasca <mdepasca@eso.org>
2015-04-15 17:26:22 +0200

    Added messages.

--- diffuse_and_classify.R
+++ diffuse_and_classify.R
@@ -15,9 +15,10 @@ eps.grid <- as.vector(seq(from=2, to=5, by=0.2))
 
 dmap <- calc_diffusion_map(path.distance, eps.val=eps.grid[2], neigen=neigen)
 
+message(paste('Building training set from', paste(path, specific.path, '/', sep='')))
 trainingSet <- get_training(path=paste(path, specific.path, '/', sep=''), fileNameRoot=specific.path)
 
-testSet <- get_test(path=paste(path, specific.path, '/', sep=''), 
-	fileNameRoot=specific.path)
+message(paste('Building test set from', paste(path, specific.path, '/', sep='')))
+testSet <- get_test(path=paste(path, specific.path, '/', sep=''), fileNameRoot=specific.path)
 
 sn.rf <- classify_RF(dmap, trainingSet, testSet)
