e00765b1999a925c74a588ab01ea301ee36490c0
mdepasca <mdepasca@eso.org>
2015-04-17 16:59:28 +0200

    Diminished number of eigenvalues. Incremented `eps.val` to be coherent with definition of big distance in distance matrix.

--- diffuse_and_classify.R
+++ diffuse_and_classify.R
@@ -5,15 +5,19 @@ source('classify_RF.R')
 
 path <- 'results/SIMGEN_PUBLIC_FIT/'
 ## specific.path <- 'RATQUAD-with_prior'
-## specific.path <- 'RBF-with_prior'
-specific.path <- 'RBF_test-length'
+specific.path <- 'RBF-with_prior'
+## specific.path <- 'RBF_test-length'
 
 path.distance <- paste(path, specific.path, '/distance_matrix/', sep='')
 
-neigen <- 120
+neigen <- 50
 eps.grid <- as.vector(seq(from=2, to=5, by=0.2))
 
-dmap <- calc_diffusion_map(path.distance, eps.val=3, neigen=neigen)
+message(paste('Data from directory:', paste(path, specific.path, sep='')))
+
+## eps.val should be near the value for which the Euclidean distance is considered too big
+## for the 2 light curves to be similar. In this case is 5
+dmap <- calc_diffusion_map(path.distance, eps.val=5, neigen=neigen)
 
 message(paste('Building training set from', paste(path, specific.path, '/', sep='')))
 trainingSet <- get_training(path=paste(path, specific.path, '/', sep=''), fileNameRoot=specific.path)
