c3d9b9a42c6c905227e37bdf3a1229ec13c9e7a8
mdepasca <mdepasca@eso.org>
2015-04-24 09:28:51 +0200

    Added delation of variables in case of existence before new assignment. To avoid appending of new version at the old variable.

--- diffuse_and_classify.R
+++ diffuse_and_classify.R
@@ -17,12 +17,24 @@ message(paste('Data from directory:', paste(path, specific.path, sep='')))
 
 ## eps.val should be near the value for which the Euclidean distance is considered too big
 ## for the 2 light curves to be similar. In this case is 5
-dmap <- calc_diffusion_map(path.distance, eps.val=5, neigen=neigen)
+eps.val <- 10
+if (exists('dmap')){
+    ## to avoid any possiblity of duplicating the size of dmap
+    rm(dmap)
+}
+dmap <- calc_diffusion_map(path.distance, eps.val=eps.val, neigen=neigen)
 
+if (exists('trainingSet')){
+    ## if the variable exists, assignment as below would only
+    ## append a new version of it
+    rm(trainingSet)
+}
 message(paste('Building training set from', paste(path, specific.path, '/', sep='')))
 trainingSet <- get_training(path=paste(path, specific.path, '/', sep=''), fileNameRoot=specific.path)
 
+if (exists('testSet')){ rm(testSet) }
 message(paste('Building test set from', paste(path, specific.path, '/', sep='')))
 testSet <- get_test(path=paste(path, specific.path, '/', sep=''), fileNameRoot=specific.path)
 
+if (exists('sn.rf')){ rm(sn.rf) }
 sn.rf <- classify_RF(dmap, trainingSet, testSet)
